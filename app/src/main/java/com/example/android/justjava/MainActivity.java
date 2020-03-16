73cc30c75d9f4f448580850633c657f83e48f924
Teselea Gheorghita <ghteselea@gmail.com>
2020-03-16 18:43:58 +0200

    Added the Adapter and the RecyclerView

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,42 +15,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.view.View;
 
-import java.text.NumberFormat;
-import java.util.Locale;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
 //    Properties
-    TextView pizzaCasTv;
-    Button plusPizzaCasBtn;
-    Button minusPizzaCasBtn;
-    TextView pizzaMexTv;
-    Button plusPizzaMexBtn;
-    Button minusPizzaMexBtn;
-    TextView pizzaCapTv;
-    Button plusPizzaCapBtn;
-    Button minusPizzaCapBtn;
-    TextView priceTextView;
+    TextView totalTextView;
     Button submitOrderBtn;
     Button sendOrderBtn;
 
-    /*
-    * TODO -> Momentan, putem comanda doar un tip de cafea.
-    * Ce ne dorim:
-    * -> In loc de cafea, vreau sa se comande pizza de pe aplicatia noastra
-    * -> nu vom avea un singur tip de pizza, ci mai multe !!!!!!!
-    * */
-
-    private int pizzaCasQuantity = 0;
-    private int pizzaMexQuantity = 0;
-    private int pizzaCapQuantity = 0;
     private int totalPrice = 0;
+    private String finalText = "";
+
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<Integer> mImageUrls = new ArrayList<>();
+    private ArrayList<Integer> mQuantities = new ArrayList<>();
+    private ArrayList<Integer> mPrices = new ArrayList<>();
 
     private static final String TAG = "MainActivity";
 
@@ -59,49 +43,24 @@ public class MainActivity extends AppCompatActivity {
 
         setup();
         addListeners();
+        initAdapter();
     }
 
 //    Custom methods
     private void setup() {
-
-        pizzaCasTv = findViewById(R.id.pizzaCasTv);
-        plusPizzaCasBtn = findViewById(R.id.plusPizzaCasBtn);
-        minusPizzaCasBtn = findViewById(R.id.minusPizzaCasBtn);
-
-
-
-        priceTextView = findViewById(R.id.price_value);
+        totalTextView = findViewById(R.id.price_value);
         submitOrderBtn = findViewById(R.id.submitBtn);
         sendOrderBtn= findViewById(R.id.sendEmailBtn);
-//        TODO: - Setup for sendEmailBtn
     }
 
     private void addListeners() {
-        plusPizzaCasBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pizzaCasQuantity++;
-                pizzaCasTv.setText(String.valueOf(pizzaCasQuantity));
-                sendOrderBtn.setEnabled(false);
-            }
-        });
-
-        minusPizzaCasBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (pizzaCasQuantity > 0) {
-                    pizzaCasQuantity--;
-                    pizzaCasTv.setText(String.valueOf(pizzaCasQuantity));
-                    sendOrderBtn.setEnabled(false);
-                }
-            }
-        });
-
         submitOrderBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                totalPrice = pizzaCasQuantity * 25 + pizzaCapQuantity * 30 + pizzaMexQuantity * 28;
-                displayPrice(totalPrice);
+
+                createFinalString();
+                totalTextView.setText(finalText);
+
                 if(totalPrice > 0 ) {
                     sendOrderBtn.setVisibility(View.VISIBLE);
                     sendOrderBtn.setEnabled(true);
@@ -112,28 +71,23 @@ public class MainActivity extends AppCompatActivity {
         sendOrderBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showAlert("Ësti sigur ca vrei sa finalizezi comanda?");
+                showAlert("Esti sigur ca vrei sa finalizezi comanda?");
             }
         });
-    }
-
-
-    private void displayPrice(int number){
-        priceTextView.setText(NumberFormat.getCurrencyInstance(new Locale("ro_RO", "RO")).format(number));
     }
 
     private void sendEmail() {
 
         String email = "gteselea@yahoo.com";
 
+        createFinalString();
+
         Intent emailIntent = new Intent(
                 Intent.ACTION_SENDTO,
                 Uri.parse("mailto:" + email));
 
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Comanda mea");
-        emailIntent.putExtra(Intent.EXTRA_TEXT,
-                "Doresc sa comand " + totalPrice + " cafele." +
-                        "\nTotal de plata: " + priceTextView.getText());
+        emailIntent.putExtra(Intent.EXTRA_TEXT, finalText);
         startActivity(Intent.createChooser(emailIntent, "Alege ce vrei sa folosesti"));
     }
 
@@ -143,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         alert.setCancelable(true);
 
         alert.setPositiveButton(
-                "Yes",
+                "DA",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         dialog.cancel();
@@ -152,17 +106,88 @@ public class MainActivity extends AppCompatActivity {
                 });
 
         alert.setNegativeButton(
-                "No",
+                "NU",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         dialog.cancel();
-                        Toast.makeText(MainActivity.this, "Ätunci esti bulangiu", Toast.LENGTH_LONG).show();
+                        Toast.makeText(MainActivity.this, "Atunci esti un bulangiu!", Toast.LENGTH_LONG).show();
                     }
                 });
 
         AlertDialog alertDialog = alert.create();
         alertDialog.show();
     }
+
+    void createFinalString() {
+//        totalPrice = pizzaCasQuantity * 25 + pizzaMexQuantity * 28 + pizzaCapQuantity * 30;
+//
+//        finalText = "Comanda: ";
+//        finalText = pizzaCasQuantity == 0 ? finalText : finalText + "\n- " + pizzaCasQuantity + " pizza Casei";
+//        finalText = pizzaMexQuantity == 0 ? finalText : finalText + "\n- " + pizzaMexQuantity + " pizza Mexicana ";
+//        finalText = pizzaCapQuantity == 0 ? finalText : finalText + "\n- " + pizzaCapQuantity + " pizza Capriciosa";
+        //if (pizzaCapQuantity > 0) {
+        //   finalText = finalText + "\n- " + pizzaCapQuantity + " pizza Capriciosa";
+        //}
+        finalText = finalText +
+                "\nTotal de plata: " + totalPrice;
+    }
+
+    private void initAdapter() {
+        Log.d(TAG, "initAdapter: preparing bitmaps.");
+
+        mImageUrls.add(R.drawable.capriciosa);
+        mNames.add("Pizza Capriciosa");
+        mQuantities.add(0);
+        mPrices.add(25);
+
+        mImageUrls.add(R.drawable.morgana);
+        mNames.add("Pizza cu mucegai");
+        mQuantities.add(0);
+        mPrices.add(30);
+
+        mImageUrls.add(R.drawable.vegetariana);
+        mNames.add("Pizza da vegani");
+        mQuantities.add(0);
+        mPrices.add(20);
+
+        mImageUrls.add(R.drawable.capriciosa);
+        mNames.add("Pizza cu \'d\' in loc de al doilea \'z\'");
+        mQuantities.add(0);
+        mPrices.add(24);
+
+        mImageUrls.add(R.drawable.capriciosa);
+        mNames.add("Pizza Capriciosa");
+        mQuantities.add(0);
+        mPrices.add(25);
+
+        mImageUrls.add(R.drawable.morgana);
+        mNames.add("Pizza cu mucegai");
+        mQuantities.add(0);
+        mPrices.add(30);
+
+        mImageUrls.add(R.drawable.vegetariana);
+        mNames.add("Pizza da vegani");
+        mQuantities.add(0);
+        mPrices.add(20);
+
+        mImageUrls.add(R.drawable.capriciosa);
+        mNames.add("Pizza cu \'d\' in loc de al doilea \'z\'");
+        mQuantities.add(0);
+        mPrices.add(24);
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        Log.d(TAG, "initRecyclerVIew: init RecyclerViwe");
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mNames, mImageUrls, mQuantities, mPrices, MainActivity.this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
+
 
 }
 
---
+++ app/src/main/java/com/example/android/justjava/RecyclerViewAdapter.java
@@ -0,0 +1,107 @@
+package com.example.android.justjava;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerViewAdapter";
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<Integer> mImages = new ArrayList<>();
+    private ArrayList<Integer> mQuantities = new ArrayList<>();
+    private ArrayList<Integer> mPrices = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(ArrayList<String> mImageNames,
+                               ArrayList<Integer> mImages,
+                               ArrayList<Integer> mQuantities,
+                               ArrayList<Integer> mPrices,
+                               Context mContext) {
+        this.mImageNames = mImageNames;
+        this.mImages = mImages;
+        this.mQuantities = mQuantities;
+        this.mPrices = mPrices;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImages.get(position))
+                .into(holder.image);
+
+        holder.pizzaName.setText(mImageNames.get(position));
+        holder.pizzaQuantity.setText(mQuantities.get(position).toString());
+        holder.pizzPrice.setText(mPrices.get(position).toString());
+
+        holder.plusPizzaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer currentValueAtPosition = mQuantities.get(position);
+                mQuantities.set(position, ++currentValueAtPosition);
+                notifyItemChanged(position);
+            }
+        });
+
+        holder.minusPizzaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer currentValueAtPosition = mQuantities.get(position);
+                mQuantities.set(position, --currentValueAtPosition);
+                notifyItemChanged(position);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        CircleImageView image;
+        TextView pizzaName;
+        TextView pizzaQuantity;
+        TextView pizzPrice;
+        Button minusPizzaBtn;
+        Button plusPizzaBtn;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            pizzaName = itemView.findViewById(R.id.pizzaName);
+            pizzaQuantity = itemView.findViewById(R.id.pizzaQuantity);
+            pizzPrice = itemView.findViewById(R.id.pizzaPrice);
+            minusPizzaBtn = itemView.findViewById(R.id.minusPizzaBtn);
+            plusPizzaBtn = itemView.findViewById(R.id.plusPizzaBtn);
+        }
+    }
+}
